e41fada8d7ee838eb5d2e52be277eeddaf08197c
osohacker1 <83740121+osohacker1@users.noreply.github.com>
2021-07-06 13:34:20 -0700

    Initial commit

---
+++ discordspam.py
@@ -0,0 +1,40 @@
+print ("{ECHO POR                         o͎s͎o͎h͎a͎c͎k͎e͎r͎                          ")
+
+print (" SPAMER DE BOT ")
+
+print ("SIGUAMEN EN MIS REDES SOCIALES!")
+
+print ("MI YOUTUBE: 1OSOHACKER1 ")
+
+print ("MI TIKTOK: 1osohacker1")
+
+print ("NO ME AGO RESPOSABLE DE COMO LO USES :) ")
+
+from typing import Text, TextIO, Type
+
+import pyautogui
+import webbrowser
+import time
+
+messeg =("“A nadie le importaba quién era yo hasta que me puse una máscara” ")
+repeats = int(input("spamer o̶s̶o̶h̶a̶c̶k̶e̶r̶  hmmm quantos mensjaes queres mandar ????????????????????????????:"))
+delay = int(input("QUANTOS MILESSEGUNDOS QUERES ESPERAR ENTRE CADA SEGUNDOS??:"))
+
+isLoaded = input("pressiona quando el discord este listo {ENETR}")
+
+
+print("tienes sinco segundos para que empize el atacke")
+
+time.sleep(5)
+
+for i in range (0,repeats):
+    if messeg != "":
+        pyautogui.typewrite(messeg)
+        pyautogui.press("enter")
+    else:
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.press("enter")
+
+    time.sleep(delay/1000)
+
+print("Done\n") 
